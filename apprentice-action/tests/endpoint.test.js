eb211794a882d63f1741a731e7175e911f9e99ad
LangBledsoe <105093232+LangBledsoe@users.noreply.github.com>
2024-10-18 00:46:37 -0700

    Changed test to use to.deep.equal

--- apprentice-action/tests/endpoint.test.js
+++ apprentice-action/tests/endpoint.test.js
@@ -34,6 +34,6 @@ describe("Tests to the \"/\" endpoint", () => {
     });
     it("should return a minified JSON object.", async () => {
         const res = await axios(`http://${dockerBridgeIP}:80/`);
-        expect(res.data).to.equal(JSON.parse(JSON.stringify(res.data)));
+        expect(res.data).to.deep.equal(JSON.parse(JSON.stringify(res.data)));
     });
 });
